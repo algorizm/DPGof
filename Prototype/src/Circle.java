e53727c9b3a6dd5b4f5377496d2da6f09bbec523
sean <sean@192.168.227.239>
2017-07-31 16:23:01 +0900

    Add Project Prototype Pattern

---
+++ Prototype/src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle extends Shape {
+	
+	public Circle() {
+		type = "Circle";
+	}
+
+	@Override
+	void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Circle::draw() method.");
+	}
+
+}
